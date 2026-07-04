4601e6b6b00a847764eb4eaca2b42a8034161cb0
Naia Sila Azachra Firdaus <naiafirdaus8@gmail.com>
2026-07-04 15:30:26 +0700

    Create news states

---
+++ lib/features/news/presentation/bloc/news_state.dart
@@ -0,0 +1,21 @@
+import '../../domain/entities/article.dart';
+
+abstract class NewsState {}
+
+class NewsLoading extends NewsState {}
+
+class NewsLoaded extends NewsState {
+
+  final List<Article> articles;
+
+  NewsLoaded(this.articles);
+
+}
+
+class NewsError extends NewsState {
+
+  final String message;
+
+  NewsError(this.message);
+
+}
