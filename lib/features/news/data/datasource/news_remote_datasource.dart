31e1491ee85cd14b301294ebc2ddaad9ec5f787b
Naia Sila Azachra Firdaus <naiafirdaus8@gmail.com>
2026-07-04 15:21:27 +0700

    Implement Dio datasource

---
+++ lib/features/news/data/datasource/news_remote_datasource.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+
+import '../model/article_model.dart';
+
+class NewsRemoteDatasource{
+
+final Dio dio;
+
+NewsRemoteDatasource(
+
+this.dio
+
+);
+
+Future<List<ArticleModel>>
+
+getArticles()
+
+async{
+
+final response = await dio.get(
+
+'https://api.spaceflightnewsapi.net/v4/articles'
+
+);
+
+return
+
+(response.data['results']
+
+as List)
+
+.map(
+
+(e)=>
+
+ArticleModel.fromJson(e)
+
+)
+
+.toList();
+
+}
+
+}
