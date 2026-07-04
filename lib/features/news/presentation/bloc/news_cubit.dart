74e320f2b18a99958f05ff0b0f355e335887071c
Naia Sila Azachra Firdaus <naiafirdaus8@gmail.com>
2026-07-04 15:56:02 +0700

    Implement NewsCubit

--- lib/features/news/presentation/bloc/news_cubit.dart
+++ lib/features/news/presentation/bloc/news_cubit.dart
@@ -1,51 +1,26 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/repositories/news_repository.dart';
-
 import 'news_state.dart';
 
-class NewsCubit extends Cubit<NewsState>{
+class NewsCubit extends Cubit<NewsState> {
+  final NewsRepository repository;
 
-final NewsRepository repository;
+  NewsCubit(this.repository) : super(NewsLoading());
 
-NewsCubit(this.repository)
+  Future<void> fetchNews() async {
+    emit(NewsLoading());
 
-: super(NewsLoading());
+    try {
+      final articles = await repository.getNews();
 
-Future<void> fetchNews()
-
-async{
-
-emit(NewsLoading());
-
-try{
-
-final articles =
-
-await repository.getNews();
-
-emit(
-
-NewsLoaded(articles)
-
-);
-
-}
-
-catch(e){
-
-emit(
-
-NewsError(
-
-e.toString()
-
-)
-
-);
-
-}
-
-}
-
+      emit(NewsLoaded(articles));
+    } catch (e) {
+      emit(
+        NewsError(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
